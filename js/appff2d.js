8f9fe2f94645b4a9228a81fcfffe789d395e9bed
Daniel <daniel.holiveira@gmail.com>
2019-03-27 14:47:04 -0300

    ultimas telas

--- js/appff2d.js
+++ js/appff2d.js
@@ -2,10 +2,13 @@
 (function() {
   var secret = '';
   var timing;
-
+ console.log('iniciou');
   otplib.authenticator.options = {
+    // 0 identifica o token atual;
+    // 1 para o token futuro (pego através do app google authenticator);
+    // -1 para o token anterior
     window: 1,
-    step: 30
+    step: 30 // tempo de duração do Token gerado
   };
 
   function toggleTabs(evt) {
@@ -24,14 +27,22 @@
   }
 
   function createSecret() {
+    // método para gerar automaticamente as chaves
     secret = otplib.authenticator.generateSecret();
 
     startCountdown();
-
+    // otplib.authenticator.keyuri(@user, @service, @secret);
+    // @user - id cliente configurado no Google APIs
+    // @service - nome do serviço configurado no Google APIs
+    // @secret - chave gerada para a aplicação criada no Google APIs
+    //
+    // após receber a chave, o método keyuri gera uma URL com o protocolo otpauth
+    // para assim gerar um novo QR CODE
     var otpauth = otplib.authenticator.keyuri('demo', 'otplib', secret);
 
     document.querySelector('.otp-secret').innerHTML = secret;
 
+    // lib QRcode utilizada para criar e exibir a imagem do QR Code gerado
     QRCode.toDataURL(otpauth, function(err, url) {
       var container = document.querySelector('.otp-qrcode .qrcode');
       if (err) {
@@ -42,10 +53,12 @@
     });
   }
 
+  // seta o token gerado na tela
   function setToken(token) {
     document.querySelector('.otp-token').innerHTML = token;
   }
 
+  // função para controlar o tempo de exibição do token na tela
   function generator() {
     if (!secret) {
       window.clearInterval(timing);
@@ -54,6 +67,7 @@
 
     const remaining = otplib.authenticator.timeRemaining();
     if (otplib.authenticator.timeUsed() === 0) {
+      // se o tempo do token expirar, é gerado um novo
       setToken(otplib.authenticator.generate(secret));
     }
 
@@ -61,20 +75,25 @@
     document.querySelector('.otp-countdown').innerHTML = remaining + 's';
   }
 
+  // função que contabiliza o tempo de vida do token
   function startCountdown() {
     window.setTimeout(() => {
       if (secret) {
+        // se existir a chave é gerado um novo token
         setToken(otplib.authenticator.generate(secret));
       }
       timing = window.setInterval(generator, 1000);
     }, 2000);
   }
 
+  // função que verifica e valida o token e a chave
   function initVerify() {
     document
       .querySelector('.otp-verify-send')
       .addEventListener('click', function() {
         var inputValue = document.querySelector('.otp-verify-input').value;
+
+        // verifica o token fornecido em relação ao token gerado
         var delta = otplib.authenticator.checkDelta(inputValue, secret);
 
         var text = document.querySelector('.otp-verify-result .text');
